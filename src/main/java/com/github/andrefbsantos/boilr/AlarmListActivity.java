f70adf0b7efb71e5ed7618d054aa6139823c3bd3
dllud <david.ludovino@gmail.com>
2014-08-02 20:20:15 +0100

    Added stub for Alarm Settings.

--- src/main/java/com/github/andrefbsantos/boilr/AlarmListActivity.java
+++ src/main/java/com/github/andrefbsantos/boilr/AlarmListActivity.java
@@ -1,23 +1,30 @@
 package com.github.andrefbsantos.boilr;
 
+import android.app.ActionBar;
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.SearchView;
 
-public class AlarmList extends Activity {
+public class AlarmListActivity extends Activity {
+
+	private ActionBar actionBar;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.alarm_list);
+		actionBar = getActionBar();
+		actionBar.setDisplayHomeAsUpEnabled(true);
 	}
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.alarm_list, menu);
-		return true;
+		SearchView searchView = (SearchView) menu.findItem(R.id.action_search).getActionView();
+		return super.onCreateOptionsMenu(menu);
 	}
 
 	@Override
@@ -26,7 +33,7 @@ public class AlarmList extends Activity {
 		// automatically handle clicks on the Home/Up button, so long
 		// as you specify a parent activity in AndroidManifest.xml.
 		int id = item.getItemId();
-		if (id == R.id.action_settings) {
+		if (id == R.id.action_search) {
 			return true;
 		}
 		return super.onOptionsItemSelected(item);
---
+++ src/main/java/com/github/andrefbsantos/boilr/AlarmSettingsActivity.java
@@ -0,0 +1,13 @@
+package com.github.andrefbsantos.boilr;
+
+import android.os.Bundle;
+import android.preference.PreferenceActivity;
+
+public class AlarmSettingsActivity extends PreferenceActivity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+	}
+
+}
