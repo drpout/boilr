1870f9ce811128c391ee50f1dd0cc4115b775594
André Santos <andrefilipebrazsantos@gmail.com>
2014-09-16 12:06:53 +0100

    Alarm creation settings updated with alert options.

--- src/main/java/com/github/andrefbsantos/boilr/views/fragments/AlarmSettingsFragment.java
+++ src/main/java/com/github/andrefbsantos/boilr/views/fragments/AlarmSettingsFragment.java
@@ -12,13 +12,13 @@ import android.preference.Preference.OnPreferenceChangeListener;
 public class AlarmSettingsFragment extends PreferenceFragment{
 
 
-	private static final String PRICE_HIT = "price_hit";
-	private static final String PRICE_VAR = "price_var";
-	private static final String EXCHANGE = "exchange";
-	private static final String TYPE = "type";
-	private static final String PAIR = "pair";
-	private static final String PREF_EXCHANGE_DEFAULT = "Bitstamp";
-	private static final String PREF_TYPE_DEFAULT = "Price Hit";
+	private static final String PREF_VALUE_PRICE_HIT = "price_hit";
+	private static final String PREF_VALUE_PRICE_VAR = "price_var";
+	private static final String PREF_KEY_EXCHANGE = "exchange";
+	private static final String PREF_KEY_TYPE = "type";
+	private static final String PREF_KEY_PAIR = "pair";
+	//private static final String PREF_EXCHANGE_DEFAULT = "Bitstamp";
+	private static final String PREF_TYPE_DEFAULT_VALUE = "Price Hit";
 
 	class OnSettingsPreferenceChangeListener implements OnPreferenceChangeListener{
 
@@ -26,36 +26,20 @@ public class AlarmSettingsFragment extends PreferenceFragment{
 		public boolean onPreferenceChange(Preference preference, Object newValue) {
 			Log.d(preference.getKey() +" " + newValue);
 
-			String summary = (String) newValue;
-			if(preference.getKey().equals(EXCHANGE)){
-				ListPreference list = (ListPreference) preference;
-				CharSequence[] values = list.getEntryValues();
-				for(int i = 0;i<values.length; i++){
-					if(values[i].equals((CharSequence) newValue)){
-						summary = (String) list.getEntries()[i];
-						break;
-					}
-				}			
-			}else if(preference.getKey().equals(PAIR)){	
-
-			}else if(preference.getKey().equals(TYPE)){
-				ListPreference list = (ListPreference) preference;
-				CharSequence[] values = list.getEntryValues();
-				for(int i = 0;i<values.length; i++){
-					if(values[i].equals((CharSequence) newValue)){
-						summary = (String) list.getEntries()[i];
-						break;
-					}
-				}
-				if(newValue.equals(PRICE_VAR)){
+			CharSequence summary = (String) newValue;
+			if(preference.getKey().equals(PREF_KEY_EXCHANGE)){
+				summary = ((ListPreference) preference).getEntry();
+				//TODO: refresh pair list
+			}else if(preference.getKey().equals(PREF_KEY_PAIR)){	
+				summary = ((ListPreference) preference).getEntry();
+				//TODO
+			}else if(preference.getKey().equals(PREF_KEY_TYPE)){
+				if(newValue.equals(PREF_VALUE_PRICE_VAR)){
 					getActivity().getFragmentManager().beginTransaction().replace(android.R.id.content, new PriceVarAlarmSettingsFragment()).commit();
-				}else if(newValue.equals(PRICE_HIT)){
+				}else if(newValue.equals(PREF_VALUE_PRICE_HIT)){
 					getActivity().getFragmentManager().beginTransaction().replace(android.R.id.content, new PriceHitAlarmSettingsFragment()).commit();
 				}
-			}else if(1==1){
-
 			}
-
 			preference.setSummary(summary);
 			return false;	
 		}
@@ -67,14 +51,30 @@ public class AlarmSettingsFragment extends PreferenceFragment{
 		addPreferencesFromResource(R.xml.alarm_settings);
 		Preference pref;
 
-		pref = findPreference("exchange");
-		pref.setOnPreferenceChangeListener(new OnSettingsPreferenceChangeListener());
-		pref.setSummary(PREF_EXCHANGE_DEFAULT);
-
-		pref = findPreference("pair");
+		pref = findPreference(PREF_KEY_EXCHANGE);
 		pref.setOnPreferenceChangeListener(new OnSettingsPreferenceChangeListener());
 
-		pref = findPreference("type");
+		//First Entry as default
+		CharSequence defaultEntry = ((ListPreference)pref).getEntries()[0]; 
+		pref.setSummary(defaultEntry);
+
+		pref = findPreference(PREF_KEY_PAIR);
 		pref.setOnPreferenceChangeListener(new OnSettingsPreferenceChangeListener());
+
+
+		pref = findPreference(PREF_KEY_PAIR);
+		pref.setOnPreferenceChangeListener(new OnSettingsPreferenceChangeListener());
+		
+		
+		//SharedPreferences sharedPreferences = getPreferenceScreen().getSharedPreferences();
+//		SharedPreferences sharedPreferences = 	PreferenceManager.getDefaultSharedPreferences(this.getActivity());
+//		
+//		RingtonePreference alertSoundPref = (RingtonePreference) findPreference(SettingsFragment.PREF_KEY_DEFAULT_ALERT_SOUND);
+//		ListPreference alertTypePref = (ListPreference) findPreference(SettingsFragment.PREF_KEY_DEFAULT_ALERT_TYPE);
+//		
+//		alertSoundPref.setRingtoneType(Integer.parseInt(alertTypePref.getValue()));
+//		alertSoundPref.setSummary(ringtoneUriToName(sharedPreferences.getString(SettingsFragment.PREF_KEY_DEFAULT_ALERT_SOUND, "")));
+		
+		
 	}
 }
