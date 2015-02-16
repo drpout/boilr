c5362ac2148937ba7fae7af79dce1b684ecd6ef2
dllud <david.ludovino@gmail.com>
2015-02-16 00:13:24 +0000

    Added click listener to lower limit and change in alarm row, allowing direct value input. Closes #48.

--- src/main/java/mobi/boilr/boilr/widget/AlarmListAdapter.java
+++ src/main/java/mobi/boilr/boilr/widget/AlarmListAdapter.java
@@ -8,6 +8,7 @@ import mobi.boilr.boilr.listeners.SwipeAndMoveTouchListener.Reference;
 import mobi.boilr.boilr.utils.Conversions;
 import mobi.boilr.boilr.views.fragments.PriceHitAlarmSettingsFragment;
 import mobi.boilr.libpricealarm.Alarm;
+import mobi.boilr.libpricealarm.PriceChangeAlarm;
 import mobi.boilr.libpricealarm.PriceHitAlarm;
 import mobi.boilr.libpricealarm.RollingPriceChangeAlarm;
 import mobi.boilr.libpricealarm.UpperLimitSmallerOrEqualLowerLimitException;
@@ -18,22 +19,22 @@ import android.view.DragEvent;
 import android.view.View;
 import android.view.View.OnDragListener;
 import android.view.ViewGroup;
+import android.view.WindowManager;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
 public class AlarmListAdapter extends ListAdapter<Alarm> {
 	private boolean mStarted = false;
-	private AlarmListActivity mAlarmListActivity;
+	private AlarmListActivity alarmListActivity;
 	private TextView mExchange;
 	private TextView mPair;
 	private AlertDialog.Builder mAlertBuilder;
 	private EditText mTextInput;
-	private Alarm mAlarm;
 	
 	public AlarmListAdapter(AlarmListActivity alarmListActivity, List<Alarm> alarms) {
 		super(alarmListActivity, alarms);
-		this.mAlarmListActivity = alarmListActivity;
+		this.alarmListActivity = alarmListActivity;
 	}
 
 	@Override
@@ -50,13 +51,19 @@ public class AlarmListAdapter extends ListAdapter<Alarm> {
 			progressCircle.setOnClickListener(new View.OnClickListener() {
 				@Override
 				public void onClick(View v) {
-					mAlarmListActivity.getStorageAndControlService().toggleAlarm(((AlarmLayout) v.getParent().getParent()).getAlarm().getId());
+					alarmListActivity.getStorageAndControlService().toggleAlarm(((AlarmLayout) v.getParent().getParent()).getAlarm().getId());
 				}
 			});
 			convertView.findViewById(R.id.upper_limit).setOnClickListener(new View.OnClickListener() {
 				@Override
 				public void onClick(View v) {
 					upperLimitClicked(v);
+				}
+			});
+			convertView.findViewById(R.id.lower_limit).setOnClickListener(new View.OnClickListener() {
+				@Override
+				public void onClick(View v) {
+					lowerLimitClicked(v);
 				}
 			});
 			convertView.setOnDragListener(new OnDragListener() {
@@ -99,6 +106,17 @@ public class AlarmListAdapter extends ListAdapter<Alarm> {
 				convertView.setTranslationX(0);
 		}
 
+		if(alarm instanceof PriceChangeAlarm) {
+			convertView.findViewById(R.id.change_base_wrapper).setClickable(true);
+			convertView.findViewById(R.id.change_base_wrapper).setOnClickListener(new View.OnClickListener() {
+				@Override
+				public void onClick(View v) {
+					changeClicked(v);
+				}
+			});
+		} else {
+			convertView.findViewById(R.id.change_base_wrapper).setClickable(false);
+		}
 		mExchange = (TextView) convertView.findViewById(R.id.exchange);
 		mExchange.setText(alarm.getExchange().getName());
 		mPair = (TextView) convertView.findViewById(R.id.pair);
@@ -112,23 +130,14 @@ public class AlarmListAdapter extends ListAdapter<Alarm> {
 	@Override
 	public void moveTo(Alarm A1, Alarm A2) {
 		super.moveTo(A1, A2);
-		mAlarmListActivity.getStorageAndControlService().updateAlarmPosition(A1, A2);
+		alarmListActivity.getStorageAndControlService().updateAlarmPosition(A1, A2);
 	}
 	
-	private void prepareDialog(View v) {
-		mAlertBuilder = new AlertDialog.Builder(mAlarmListActivity);
-		mTextInput = new EditText(mAlarmListActivity);
-		mTextInput.setInputType(InputType.TYPE_CLASS_NUMBER | InputType.TYPE_NUMBER_FLAG_DECIMAL);
-		mAlertBuilder.setView(mTextInput);
-		mAlarm = ((AlarmLayout) v.getParent().getParent()).getAlarm();
-	}
-
 	private void upperLimitClicked(View v) {
-		prepareDialog(v);
-		if(mAlarm instanceof PriceHitAlarm) {
-			final PriceHitAlarm priceHitAlarm = (PriceHitAlarm) mAlarm;
-			String formated = Conversions.formatMaxDecimalPlaces(priceHitAlarm.getUpperLimit());
-			mTextInput.setText(formated);
+		Alarm alarm = ((AlarmLayout) v.getParent().getParent()).getAlarm();
+		if(alarm instanceof PriceHitAlarm) {
+			final PriceHitAlarm priceHitAlarm = (PriceHitAlarm) alarm;
+			prepareDialog(priceHitAlarm.getUpperLimit());
 			mAlertBuilder.setTitle(R.string.pref_title_upper_limit);
 			mAlertBuilder.setPositiveButton(R.string.ok, new DialogInterface.OnClickListener() {
 				@Override
@@ -140,22 +149,85 @@ public class AlarmListAdapter extends ListAdapter<Alarm> {
 					}
 				}
 			});
-			mAlertBuilder.setNegativeButton(R.string.cancel, new DialogInterface.OnClickListener() {
+			showDialog();
+		} else if(alarm instanceof RollingPriceChangeAlarm) {
+			RollingPriceChangeAlarm changeAlarm = (RollingPriceChangeAlarm) alarm;
+			showLimitExplantion(changeAlarm, true);
+		}
+	}
+
+	private void lowerLimitClicked(View v) {
+		Alarm alarm = ((AlarmLayout) v.getParent().getParent()).getAlarm();
+		if(alarm instanceof PriceHitAlarm) {
+			final PriceHitAlarm priceHitAlarm = (PriceHitAlarm) alarm;
+			prepareDialog(priceHitAlarm.getLowerLimit());
+			mAlertBuilder.setTitle(R.string.pref_title_lower_limit);
+			mAlertBuilder.setPositiveButton(R.string.ok, new DialogInterface.OnClickListener() {
 				@Override
 				public void onClick(DialogInterface dialog, int whichButton) {
-					// Canceled, do nothing.
+					try {
+						priceHitAlarm.setLowerLimit(Double.parseDouble(mTextInput.getText().toString()));
+					} catch(UpperLimitSmallerOrEqualLowerLimitException e) {
+						PriceHitAlarmSettingsFragment.handleLimitsExceptions(e, getContext());
+					}
 				}
 			});
-			mAlertBuilder.show();
-		} else if(mAlarm instanceof RollingPriceChangeAlarm) {
-			RollingPriceChangeAlarm changeAlarm = (RollingPriceChangeAlarm) mAlarm;
-			String change = changeAlarm.isPercent() ? Conversions.format2DecimalPlaces(changeAlarm.getPercent()) + "%" :
-				Conversions.formatMaxDecimalPlaces(changeAlarm.getChange());
-			Toast.makeText(getContext(), getContext().getString(R.string.limit_explanation, 
-					Conversions.formatMaxDecimalPlaces(changeAlarm.getUpperLimit()),
-					Conversions.formatMaxDecimalPlaces(changeAlarm.getBaseValue()),
-					Conversions.formatMilis(changeAlarm.getTimeFrame(), getContext()),
-					change), Toast.LENGTH_LONG).show();
+			showDialog();
+		} else if(alarm instanceof RollingPriceChangeAlarm) {
+			RollingPriceChangeAlarm changeAlarm = (RollingPriceChangeAlarm) alarm;
+			showLimitExplantion(changeAlarm, false);
 		}
+	}
+
+	private void changeClicked(View v) {
+		final RollingPriceChangeAlarm changeAlarm = (RollingPriceChangeAlarm) ((AlarmLayout) v.getParent().getParent()).getAlarm();
+		if(changeAlarm.isPercent())
+			prepareDialog(changeAlarm.getPercent());
+		else
+			prepareDialog(changeAlarm.getChange());
+		mAlertBuilder.setTitle(R.string.pref_title_change_value);
+		mAlertBuilder.setPositiveButton(R.string.ok, new DialogInterface.OnClickListener() {
+			@Override
+			public void onClick(DialogInterface dialog, int whichButton) {
+				double newValue = Double.parseDouble(mTextInput.getText().toString());
+				if(changeAlarm.isPercent())
+					changeAlarm.setPercent((float) newValue);
+				else
+					changeAlarm.setChange(newValue);
+			}
+		});
+		showDialog();
+	}
+
+	private void prepareDialog(double currValue) {
+		mAlertBuilder = new AlertDialog.Builder(alarmListActivity);
+		mTextInput = new EditText(alarmListActivity);
+		mTextInput.setInputType(InputType.TYPE_CLASS_NUMBER | InputType.TYPE_NUMBER_FLAG_DECIMAL);
+		mAlertBuilder.setView(mTextInput);
+		mAlertBuilder.setNegativeButton(R.string.cancel, new DialogInterface.OnClickListener() {
+			@Override
+			public void onClick(DialogInterface dialog, int whichButton) {
+				// Canceled, do nothing.
+			}
+		});
+		mTextInput.setText(Conversions.formatMaxDecimalPlaces(currValue));
+	}
+
+	private void showDialog() {
+		AlertDialog dialog = mAlertBuilder.show();
+		dialog.getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_ALWAYS_VISIBLE);
+	}
+
+	private void showLimitExplantion(RollingPriceChangeAlarm changeAlarm, boolean isUpperLimit) {
+		String change = changeAlarm.isPercent() ? Conversions.format2DecimalPlaces(changeAlarm.getPercent()) + "%" :
+			Conversions.formatMaxDecimalPlaces(changeAlarm.getChange());
+		String limit = Conversions.formatMaxDecimalPlaces(isUpperLimit ? changeAlarm.getUpperLimit() : changeAlarm.getLowerLimit());
+		String op = isUpperLimit ? "+" : "-";
+		Toast.makeText(getContext(), getContext().getString(R.string.limit_explanation, 
+				limit,
+				Conversions.formatMaxDecimalPlaces(changeAlarm.getBaseValue()),
+				Conversions.formatMilis(changeAlarm.getTimeFrame(), getContext()),
+				op,
+				change), Toast.LENGTH_LONG).show();
 	}
 }
