3861d9fddeb79a880f9c249e30266db872bcf325
dllud <david.ludovino@gmail.com>
2015-03-05 05:23:24 +0000

    Using light theme no internet icon for Lollipop to match the new white notifications' background.

--- src/main/java/mobi/boilr/boilr/utils/Notifications.java
+++ src/main/java/mobi/boilr/boilr/utils/Notifications.java
@@ -20,6 +20,7 @@ import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
 import android.graphics.Paint;
 import android.media.RingtoneManager;
+import android.os.Build;
 
 /* Based on Android DeskClock AlarmNotifications. */
 public final class Notifications {
@@ -139,7 +140,12 @@ public final class Notifications {
 				changeSettingsIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
 				Intent disableIntent = new Intent(context, StorageAndControlService.class);
 				disableIntent.setAction(Notifications.ACTION_DISABLE_NET_NOTIF);
-				Bitmap largeIcon = BitmapFactory.decodeResource(context.getResources(), R.drawable.ic_no_wifi_dark);
+				int icNoWifi;
+				if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP)
+					icNoWifi = R.drawable.ic_no_wifi_light;
+				else
+					icNoWifi = R.drawable.ic_no_wifi_dark;
+				Bitmap largeIcon = BitmapFactory.decodeResource(context.getResources(), icNoWifi);
 				sNoNetNotif = new Notification.Builder(context)
 						.setContentTitle(context.getString(R.string.check_connection))
 						.setContentText(context.getString(R.string.alarms_not_updating))
